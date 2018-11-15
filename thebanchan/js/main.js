aae400d4ae3f8d937bfcbef7b258a85b4f9e83e1
Lo <lothian.taylor@gmail.com>
2018-11-15 19:24:23 -0400

    delete all the things

--- thebanchan/js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  console.log("Hello from main.js!")
-  $(".close, .hamburger").click(function(e){
-    e.preventDefault();
-    $(".main-nav").toggleClass('open-nav');
-  });
-});
